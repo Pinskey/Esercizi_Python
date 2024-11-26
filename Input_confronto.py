e3eb5ad8ec107660f8b5b6e18a15eed4a0b12d3b
Pinskey <francescopisaneschi.lavoro@gmail.com>
2024-11-26 11:55:45 +0100

    mod 4

---
+++ Input_confronto.py
@@ -0,0 +1,18 @@
+# Input e confronto
+
+x = int(input("Inserisci x (numero intero) "))
+if x == 3:
+    print("x è uguale a 3")
+elif x<3:
+    print("x è minore di 3")
+else:
+    print("x è maggiore di 3")
+
+y = int(input("inserisci y (numero intero) "))
+
+if (x == y):
+    print("x e y sono uguali")
+elif(x>y):
+    print("x è maggiore di y")
+else:
+    print("x è minore di y")
