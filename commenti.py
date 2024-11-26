160e35e024dc36634092731ca05b83e274e2884d
Pinskey <francescopisaneschi.lavoro@gmail.com>
2024-11-26 12:01:22 +0100

    mod 7

---
+++ commenti.py
@@ -0,0 +1,5 @@
+# Commenti
+
+frutta = ["mela", "banana", "uva", "pera", "melone","ananas"]
+for fruit in frutta: # Usando in il ciclo for va a pescare ogni elemento della lista
+    print(fruit) # il print stamperà quindi ogni elemento della lista
