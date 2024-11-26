762356c8c7df6ab66360c7a60e687c50d1bd1022
Pinskey <francescopisaneschi.lavoro@gmail.com>
2024-11-26 11:58:02 +0100

    mod 5

---
+++ for+ while.py
@@ -0,0 +1,9 @@
+# Ciclo for e while
+
+frutta = ["mela", "banana", "uva", "pera", "melone","ananas"]
+i=0
+while i < len(frutta):
+    for carattere in frutta[i]:
+        print(carattere)
+    print("\n")
+    i += 1
